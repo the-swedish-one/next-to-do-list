142c81ccb00d1411b9753627754076d7510bc90c
the-swedish-one <a.damgaard@oxon.org>
2023-08-02 16:09:45 +0200

    Add link with styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <header>
-        <h1 className="text-2xl">To Do List</h1>;
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">To Do List</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
       </header>
-      <ul></ul>
+      <ul className="pl-4"></ul>
     </>
   );
 }
